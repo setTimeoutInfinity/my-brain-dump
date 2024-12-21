b840461541b8a6b5ec843a0964f395fabf191742
setTimeoutInfinity <dan.lakzian@gmail.com>
2024-12-22 08:37:33 +1100

    Initial commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Barlow", "sans-serif"],
+        serif: ["Rubik", "serif"],
+        display: ["Barlow", "sans-serif"],
+        body: ["Rubik", "sans-serif"],
+      },
+    },
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["light", "dark"],
+  },
+};

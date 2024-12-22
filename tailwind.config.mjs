0eb9557030c0d8a0d7cc1242015d260b537ad538
setTimeoutInfinity <dan.lakzian@gmail.com>
2024-12-22 21:40:41 +1100

    feat: add blog page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -11,7 +11,7 @@ export default {
       },
     },
   },
-  plugins: [require("daisyui")],
+  plugins: [require("daisyui"), require("@tailwindcss/typography")],
   daisyui: {
     themes: ["light", "dark"],
   },

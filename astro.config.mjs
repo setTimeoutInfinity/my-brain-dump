1e684f05d5f0497f26cd372fee7df75be411fa71
setTimeoutInfinity <dan.lakzian@gmail.com>
2024-12-24 11:09:47 +1100

    chore: add site config to astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,7 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://lakzian.com",
   integrations: [tailwind(), react(), mdx()],
   experimental: {
     svg: {

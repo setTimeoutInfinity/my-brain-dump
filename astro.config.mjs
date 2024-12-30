2c487ca74b49e91382a611cd20dab610d3b52dd8
setTimeoutInfinity <dan.lakzian@gmail.com>
2024-12-31 10:39:22 +1100

    feat: integrate node adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,13 +9,21 @@ import mdx from "@astrojs/mdx";
 
 import sitemap from "@astrojs/sitemap";
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://lakzian.com",
   integrations: [tailwind(), react(), mdx(), sitemap()],
+  output: "server",
+
   experimental: {
     svg: {
       mode: "inline",
     },
   },
+
+  adapter: node({
+    mode: "standalone",
+  }),
 });
